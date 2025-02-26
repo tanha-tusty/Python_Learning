a66e6ea0b58296dd3d42612228641286239316d1
Tanha Sultana Tusty <tanhatusty1@gmail.com>
2025-02-26 18:23:29 +0600

    Create list_range.py

---
+++ list_range.py
@@ -0,0 +1,11 @@
+fav_cartoon = ['tom_jerry','pooh','looney_tunes','Mickey','ben_10','power_rangers']
+print(fav_cartoon)
+
+# range
+print(fav_cartoon[0:3])
+print(len(fav_cartoon))
+
+fav_food_items = ['burger','pizza','pasta','sandwich','french fries','wedges']
+print(fav_food_items)
+
+print(fav_food_items[0:4])
